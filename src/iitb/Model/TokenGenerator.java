c7a0d43ca5fe0d05d7cca5099abb5e4377089331
sunita <sunita>
2004-07-11 04:06:54 +0000

    Arbitrary tokenization

---
+++ src/iitb/Model/TokenGenerator.java
@@ -0,0 +1,9 @@
+package iitb.Model;
+
+public class TokenGenerator {
+    boolean firstCall;
+    Object x;
+    public void startScan(Object xArg) {x=xArg; firstCall=true;}
+    public boolean hasNext() {return firstCall;}
+    public Object next() {firstCall = false; return x;}
+};
