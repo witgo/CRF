da555f70174d229caedb25c29f2f9f378c9753e4
sunita <sunita>
2004-11-12 10:10:11 +0000

    Changed indentation

--- src/iitb/Model/FeatureTypesEachLabel.java
+++ src/iitb/Model/FeatureTypesEachLabel.java
@@ -3,46 +3,55 @@ package iitb.Model;
  * This can be used as a wrapper around a FeatureTypes class that wants to
  * generate a feature for each label. 
  */
-public class  FeatureTypesEachLabel extends FeatureTypes {
-    FeatureTypes single;
-    int numStates;
-    int stateId;
-    FeatureImpl featureImpl;
-    public FeatureTypesEachLabel(Model m, FeatureTypes single) {
-	super(m);
-	numStates = model.numStates();
-	this.single = single;
-	featureImpl = new FeatureImpl();
-    }
-    boolean advance() {
-	stateId++;
-	if (stateId < numStates)
-	    return true;
-	if (single.hasNext()) {
-	    single.next(featureImpl);
-	    stateId = 0;
-	} else {
-	    stateId=numStates;
-	    return false;
+public class FeatureTypesEachLabel extends FeatureTypes {
+	FeatureTypes single;
+
+	int numStates;
+
+	int stateId;
+
+	FeatureImpl featureImpl;
+
+	public FeatureTypesEachLabel(Model m, FeatureTypes single) {
+		super(m);
+		numStates = model.numStates();
+		this.single = single;
+		featureImpl = new FeatureImpl();
 	}
-	return true;
-    }
-    public  boolean startScanFeaturesAt(iitb.CRF.DataSequence data, int prevPos, int pos) {
-	stateId = numStates;
-	single.startScanFeaturesAt(data,prevPos,pos);
-	return advance();
-    }
-    public boolean hasNext() {
-	return (stateId  < numStates);
-    }
-    public  void next(iitb.Model.FeatureImpl f) {
-	f.copy(featureImpl);
-	f.yend = stateId;
-	single.setFeatureIdentifier(featureImpl.strId.id*numStates+stateId, stateId, featureImpl.strId.name, f);
-	advance();
-    }
-    int labelIndependentId(FeatureImpl f) {
-	return ((f.strId.id-single.thisTypeId)-f.strId.stateId*offset)/model.numStates()+single.thisTypeId;
-    }
+
+	boolean advance() {
+		stateId++;
+		if (stateId < numStates)
+			return true;
+		if (single.hasNext()) {
+			single.next(featureImpl);
+			stateId = 0;
+		} 
+		return stateId < numStates;
+	}
+
+	public boolean startScanFeaturesAt(iitb.CRF.DataSequence data, int prevPos,
+			int pos) {
+		stateId = numStates;
+		single.startScanFeaturesAt(data, prevPos, pos);
+		return advance();
+	}
+
+	public boolean hasNext() {
+		return (stateId < numStates);
+	}
+
+	public void next(iitb.Model.FeatureImpl f) {
+		f.copy(featureImpl);
+		f.yend = stateId;
+		single.setFeatureIdentifier(featureImpl.strId.id * numStates + stateId,
+				stateId, featureImpl.strId.name, f);
+		advance();
+	}
+
+	int labelIndependentId(FeatureImpl f) {
+		return ((f.strId.id - single.thisTypeId) - f.strId.stateId * offset)
+				/ model.numStates() + single.thisTypeId;
+	}
 };
 	
