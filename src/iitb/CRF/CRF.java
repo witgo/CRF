9098faa5f45fdfa96731176b094266a62dff5353
sunita <sunita>
2004-12-10 12:53:47 +0000

    Added support for returning parameters and included some more debug statements.

--- src/iitb/CRF/CRF.java
+++ src/iitb/CRF/CRF.java
@@ -36,76 +36,79 @@ public class CRF implements Serializable {
      * @see iitb.CRF.CrfParams 
      */
     public CRF(int numLabels, FeatureGenerator fgen, String arg) {
-	this(numLabels, fgen, CrfParams.stringToOptions(arg));
+        this(numLabels, fgen, CrfParams.stringToOptions(arg));
     }
-
+    
     public CRF(int numLabels, FeatureGenerator fgen, java.util.Properties configOptions) {
-	this(numLabels,1,fgen,configOptions);
+        this(numLabels,1,fgen,configOptions);
     }
-
+    
     public CRF(int numLabels, int histsize, FeatureGenerator fgen, java.util.Properties configOptions) {
-	histMgr = new HistoryManager(histsize,numLabels);
-	featureGenerator = histMgr.getFeatureGen(fgen);
-	numY = histMgr.numY;
-	params = new CrfParams(configOptions);
-	edgeGen = histMgr.getEdgeGenerator();
-	viterbi = new Viterbi(this,1);
+        histMgr = new HistoryManager(histsize,numLabels);
+        featureGenerator = histMgr.getFeatureGen(fgen);
+        numY = histMgr.numY;
+        params = new CrfParams(configOptions);
+        edgeGen = histMgr.getEdgeGenerator();
+        viterbi = new Viterbi(this,1);
     }
-
+    
     /**
      * write the trained parameters of the CRF to the file
      */
     public void write(String fileName)  throws IOException {
-	PrintWriter out=new PrintWriter(new FileOutputStream(fileName));
-	out.println(lambda.length);
-	for (int i = 0; i < lambda.length; i++)
-	    out.println(lambda[i]);
-	out.close();
+        PrintWriter out=new PrintWriter(new FileOutputStream(fileName));
+        out.println(lambda.length);
+        for (int i = 0; i < lambda.length; i++)
+            out.println(lambda[i]);
+        out.close();
     }
     /**
      * read the parameters of the CRF from a file
      */
     public void read(String fileName) throws IOException {
-	BufferedReader in=new BufferedReader(new FileReader(fileName));
-	int numF = Integer.parseInt(in.readLine());
-	lambda = new double[numF];
-	int pos = 0;
-	String line;
-	while((line=in.readLine())!=null) {
-	    lambda[pos++] = Double.parseDouble(line);
-	}
+        BufferedReader in=new BufferedReader(new FileReader(fileName));
+        int numF = Integer.parseInt(in.readLine());
+        lambda = new double[numF];
+        int pos = 0;
+        String line;
+        while((line=in.readLine())!=null) {
+            lambda[pos++] = Double.parseDouble(line);
+        }
     }
     protected Trainer getTrainer() {
-	if (params.trainerType.startsWith("Collins"))
-	    return new CollinsTrainer(params);
-	return new Trainer(params);
+        if (params.trainerType.startsWith("Collins"))
+            return new CollinsTrainer(params);
+        return new Trainer(params);
     }
     protected Viterbi getViterbi(int beamsize) {
-    	return new Viterbi(this,beamsize);
+        return new Viterbi(this,beamsize);
     }
     /**
      * Trains the model given the data
      * @return the learnt parameter value as an array
      */
     public double[] train(DataIter trainData) {
-	return train(trainData,null);
+        return train(trainData,null);
     }
     /**
      * Trains the model given the data
      * @return the learnt parameter value as an array
      */
     public double[] train(DataIter trainData, Evaluator evaluator) {
-	lambda = new double[featureGenerator.numFeatures()];	
-	trainer = getTrainer();
-	trainer.train(this, histMgr.mapTrainData(trainData), lambda, evaluator);
-	return lambda;
+        lambda = new double[featureGenerator.numFeatures()];	
+        trainer = getTrainer();
+        trainer.train(this, histMgr.mapTrainData(trainData), lambda, evaluator);
+        return lambda;
+    }
+    public double[] learntWeights() {
+        return lambda;
     }
     public void apply(DataSequence dataSeq) {
-	if (params.debugLvl > 1) 
-	    Util.printDbg("CRF: Applying on " + dataSeq);
-	viterbi.bestLabelSequence(dataSeq,lambda);
-	for(int i = dataSeq.length()-1; i >= 0; i--) {
-	    histMgr.set_y(dataSeq, i, dataSeq.y(i));
-	}
+        if (params.debugLvl > 1) 
+            Util.printDbg("CRF: Applying on " + dataSeq);
+        viterbi.bestLabelSequence(dataSeq,lambda);
+        for(int i = dataSeq.length()-1; i >= 0; i--) {
+            histMgr.set_y(dataSeq, i, dataSeq.y(i));
+        }
     }
 };
