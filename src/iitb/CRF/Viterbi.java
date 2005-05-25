3221ba2105381ee38298ad59f5c2b3715d85f307
imansuri <imansuri>
2005-05-25 09:17:09 +0000

    adding backward viterbi serach.

--- src/iitb/CRF/Viterbi.java
+++ src/iitb/CRF/Viterbi.java
@@ -21,11 +21,10 @@ public class Viterbi implements Serializable {
     protected CRF model;
     protected int beamsize;
     Viterbi(CRF model, int bs) {
-	this.model = model;
-	beamsize = bs;
-	if (model.params.miscOptions.getProperty("beamSize") != null)
-	    beamsize = Integer.parseInt(model.params.miscOptions.getProperty("beamSize"));
-
+        this.model = model;
+        beamsize = bs;
+        if (model.params.miscOptions.getProperty("beamSize") != null)
+            beamsize = Integer.parseInt(model.params.miscOptions.getProperty("beamSize"));
     }
     protected class Entry {
         public Soln solns[]; // TODO.
@@ -50,6 +49,7 @@ public class Viterbi implements Serializable {
             for (int k = size()-1; k > i; k--) {
                 solns[k].copy(solns[k-1]);
             }
+            
             solns[i].setPrevSoln(prev,score);
         }
         protected void add(Entry e, float thisScore) {
@@ -63,7 +63,7 @@ public class Viterbi implements Serializable {
                 float score = e.get(i).score + thisScore;
                 insertPos = findInsert(insertPos, score, e.get(i));
             }
-            //	    print();
+            //print()
         }
         protected int findInsert(int insertPos, float score, Soln prev) {
             for (; insertPos < size(); insertPos++) {
@@ -91,52 +91,127 @@ public class Viterbi implements Serializable {
                 str += ("["+i + " " + solns[i].score + " i:" + solns[i].pos + " y:" + solns[i].label+"]");
             System.out.println(str);
         }
+        
+        public String toString(){
+            assert(solns != null && solns[0] != null);
+            String toString = "";
+            toString += "[" + solns[0].pos + " " + solns[0].label + " " + solns[0].score;
+            if(solns[0].prevSoln != null)
+                toString += " : " + solns[0].prevSoln.pos + " " + solns[0].prevSoln.label + " " + solns[0].prevSoln.score;
+            toString += "]";
+            return toString;
+        }
     };
-
+    
     Entry winningLabel[][];
     protected Entry finalSoln;
     protected DoubleMatrix2D Mi;
     protected DoubleMatrix1D Ri;
 
     void allocateScratch(int numY) {
-	Mi = new DenseDoubleMatrix2D(numY,numY);
-	Ri = new DenseDoubleMatrix1D(numY);
-	winningLabel = new Entry[numY][];
-	finalSoln = new Entry(beamsize,0,0);
+        Mi = new DenseDoubleMatrix2D(numY,numY);
+        Ri = new DenseDoubleMatrix1D(numY);
+        winningLabel = new Entry[numY][];
+        finalSoln = new Entry(beamsize,0,0);
     }
     double fillArray(DataSequence dataSeq, double lambda[], boolean calcScore) {
-	double corrScore = 0;
-	int numY = model.numY;
-	for (int i = 0; i < dataSeq.length(); i++) {
-	    // compute Mi.
-	    Trainer.computeLogMi(model.featureGenerator,lambda,dataSeq,i,Mi,Ri,false);
-	    for (int yi = 0; yi < numY; yi++) {
-		winningLabel[yi][i].clear();
-		winningLabel[yi][i].valid = true;
-	    }
-	    for (int yi = model.edgeGen.firstY(i); yi < numY; yi = model.edgeGen.nextY(yi,i)) {
-		if (i > 0) {
-		    for (int yp = model.edgeGen.first(yi); yp < numY; yp = model.edgeGen.next(yi,yp)) {
-			double val = Mi.get(yp,yi)+Ri.get(yi);
-			winningLabel[yi][i].add(winningLabel[yp][i-1], (float)val);
-		    }
-		} else {
-		    winningLabel[yi][i].add((float)Ri.get(yi));
-		}
-	    }
-	    if (calcScore)
-		corrScore += (Ri.get(dataSeq.y(i)) + ((i > 0)?Mi.get(dataSeq.y(i-1),dataSeq.y(i)):0));
-	}
-	return corrScore;
+        double corrScore = 0;
+        int numY = model.numY;
+        for (int i = 0; i < dataSeq.length(); i++) {
+            // compute Mi.
+            Trainer.computeLogMi(model.featureGenerator,lambda,dataSeq,i,Mi,Ri,false);
+            for (int yi = 0; yi < numY; yi++) {
+                winningLabel[yi][i].clear();
+                winningLabel[yi][i].valid = true;
+            }
+            for (int yi = model.edgeGen.firstY(i); yi < numY; yi = model.edgeGen.nextY(yi,i)) {
+                if (i > 0) {
+                    for (int yp = model.edgeGen.first(yi); yp < numY; yp = model.edgeGen.next(yi,yp)) {
+                        double val = Mi.get(yp,yi)+Ri.get(yi);
+                        winningLabel[yi][i].add(winningLabel[yp][i-1], (float)val);
+                    }
+                } else {
+                    winningLabel[yi][i].add((float)Ri.get(yi));
+                }
+            }
+            if (calcScore)
+                corrScore += (Ri.get(dataSeq.y(i)) + ((i > 0)?Mi.get(dataSeq.y(i-1),dataSeq.y(i)):0));
+        }
+        return corrScore;
+    }
+
+    public double viterbiSearchBackward(DataSequence dataSeq, double lambda[], DoubleMatrix2D Mis[], DoubleMatrix1D Ris[], boolean calcCorrectScore) {
+        if (Mi == null) {
+            allocateScratch(model.numY);
+        }
+        if ((winningLabel[0] == null) || (winningLabel[0].length < dataSeq.length())) {
+            for (int yi = 0; yi < winningLabel.length; yi++) {
+                winningLabel[yi] = new Entry[dataSeq.length()];
+                for (int l = 0; l < dataSeq.length(); l++)
+                    winningLabel[yi][l] = new Entry(beamsize, yi, l);
+            }
+        }
+        Entry firstEntries[] = new Entry[model.numY];
+        for (int yi = 0; yi < winningLabel.length; yi++) {
+            firstEntries[yi] = new Entry(1, yi, 0);	
+        }
+        double corrScore = fillArrayBackward(dataSeq, lambda,firstEntries, Mis, Ris, calcCorrectScore);
+        
+        finalSoln.clear();
+        finalSoln.valid = true;
+        for (int yi = 0; yi < model.numY; yi++) {
+            finalSoln.add(firstEntries[yi], 0);
+        }
+        return corrScore;
+    }
+
+    double fillArrayBackward(DataSequence dataSeq, double lambda[], Entry firstEntries[], DoubleMatrix2D Mis[], DoubleMatrix1D Ris[], boolean calcScore) {
+        double corrScore = 0;
+        int numY = model.numY;
+
+        for (int i = dataSeq.length() - 1; i >= 0; i--) {
+            for (int yi = 0; yi < numY; yi++) {
+                winningLabel[yi][i].clear();
+                winningLabel[yi][i].valid = true;
+                if(i == dataSeq.length() - 1)
+                    winningLabel[yi][i].add(0);
+            }
+        }    	
+        for (int i = dataSeq.length() - 1; i >= 0; i--) {
+            // compute Mi.
+            Trainer.computeLogMi(model.featureGenerator,lambda,dataSeq,i,Mi,Ri,false);
+            Mis[i].assign(Mi);
+            Ris[i].assign(Ri);
+            if(i == 0)
+                break;
+            for (int yi = model.edgeGen.firstY(i); yi < numY; yi = model.edgeGen.nextY(yi,i)) {
+                for (int yp = model.edgeGen.first(yi); yp < numY; yp = model.edgeGen.next(yi,yp)){
+                    double val = Mi.get(yp,yi)+Ri.get(yi);
+                    winningLabel[yp][i-1].add(winningLabel[yi][i], (float)val);
+                }
+            }
+            if (calcScore)
+                corrScore += (Ri.get(dataSeq.y(i)) + ((i > 0)?Mi.get(dataSeq.y(i-1),dataSeq.y(i)):0));            
+        }
+        for(int yi = 0; yi < numY; yi++){
+            firstEntries[yi].clear();
+            firstEntries[yi].valid = true;
+            firstEntries[yi].add(winningLabel[yi][0], (float)Ri.get(yi));
+        }
+        
+        return corrScore;
     }
     
     protected void setSegment(DataSequence dataSeq, int prevPos, int pos, int label) {
         dataSeq.set_y(pos, label);
     }
     public void bestLabelSequence(DataSequence dataSeq, double lambda[]) {
+        //System.out.println("Viterbi ");
         double corrScore = viterbiSearch(dataSeq, lambda,false);
+        //System.out.print(finalSoln.get(0).score + "\t");
         assignLabels(dataSeq);
     }
+    
     void assignLabels(DataSequence dataSeq) {
         Soln ybest = finalSoln.get(0);
         ybest = ybest.prevSoln;
@@ -148,29 +223,32 @@ public class Viterbi implements Serializable {
         }
         assert(pos>=0);
     }
+    
     public double viterbiSearch(DataSequence dataSeq, double lambda[], boolean calcCorrectScore) {
-	if (Mi == null) {
-	    allocateScratch(model.numY);
-	}
-	if ((winningLabel[0] == null) || (winningLabel[0].length < dataSeq.length())) {
-	    for (int yi = 0; yi < winningLabel.length; yi++) {
-		winningLabel[yi] = new Entry[dataSeq.length()];
-		for (int l = 0; l < dataSeq.length(); l++)
-		    winningLabel[yi][l] = new Entry((l==0)?1:beamsize, yi, l);
-	    }
-	}
-	
-	double corrScore = fillArray(dataSeq, lambda,calcCorrectScore);
-
-	finalSoln.clear();
-	finalSoln.valid = true;
-	for (int yi = 0; yi < model.numY; yi++) {
-	    finalSoln.add(winningLabel[yi][dataSeq.length()-1], 0);
-	}
-	return corrScore;
+        if (Mi == null) {
+            allocateScratch(model.numY);
+        }
+        if ((winningLabel[0] == null) || (winningLabel[0].length < dataSeq.length())) {
+            for (int yi = 0; yi < winningLabel.length; yi++) {
+                winningLabel[yi] = new Entry[dataSeq.length()];
+                for (int l = 0; l < dataSeq.length(); l++)
+                    winningLabel[yi][l] = new Entry((l==0)?1:beamsize, yi, l);
+            }
+        }
+        
+        double corrScore = fillArray(dataSeq, lambda,calcCorrectScore);
+        
+        finalSoln.clear();
+        finalSoln.valid = true;
+        for (int yi = 0; yi < model.numY; yi++) {
+            finalSoln.add(winningLabel[yi][dataSeq.length()-1], 0);
+        }
+        return corrScore;
     }
+    
+    
     int numSolutions() {return finalSoln.numSolns();}
     Soln getBestSoln(int k) {
-	return finalSoln.get(k).prevSoln;
+        return finalSoln.get(k).prevSoln;
     }
 };
