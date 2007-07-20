732d39e8d38957495f4be94aa037181102fcda97
sunita <sunita>
2007-07-20 14:18:38 +0000

    *** empty log message ***

--- src/iitb/MaxentClassifier/FeatureGenRecord.java
+++ src/iitb/MaxentClassifier/FeatureGenRecord.java
@@ -7,62 +7,64 @@ import iitb.CRF.*;
  */ 
 
 public class FeatureGenRecord implements FeatureGenerator {
-    int numCols;
-    int numLabels;
-    DataRecord dataRecord;
-    public int addBias=0;
-    class FeatureColumn implements Feature {
-	int colId;
-	int _y;
-	float val;
-	void copy(FeatureColumn f) {
-	    colId = f.colId;
-	    _y = f._y;
-	    val = f.val;
+	int numCols;
+	int numLabels;
+	DataRecord dataRecord;
+	public int addBias=0;
+	class FeatureColumn implements Feature {
+		int colId;
+		int _y;
+		float val;
+		void copy(FeatureColumn f) {
+			colId = f.colId;
+			_y = f._y;
+			val = f.val;
+		}
+		public int index() {return colId + _y*(numCols+addBias);}
+		public int y() {return _y;}
+		public int yprev() {return -1;}
+		public float value() {return val;}
+		public int[] yprevArray() {return null;}
+	};
+	FeatureColumn feature, featureToReturn;
+	protected FeatureGenRecord(int numColumns, int numYs) {
+		numCols =  numColumns;
+		numLabels = numYs;
+		feature = new FeatureColumn();
+		featureToReturn = new FeatureColumn(); 
 	}
-	public int index() {return colId + _y*(numCols+addBias);}
-	public int y() {return _y;}
-	public int yprev() {return -1;}
-	public float value() {return val;}
-	public int[] yprevArray() {return null;}
-    };
-    FeatureColumn feature, featureToReturn;
-    FeatureGenRecord(int numColumns, int numYs) {
-	numCols =  numColumns;
-	numLabels = numYs;
-	feature = new FeatureColumn();
-	featureToReturn = new FeatureColumn(); 
-    }
-    public int numFeatures() {return (numCols+addBias)*numLabels;}
-    public void startScanFeaturesAt(DataSequence data, int pos) {
-	dataRecord = (DataRecord)data;
-	assert (pos == 0);
-	feature.colId = 0;
-	feature._y = 0;
-    }
-    public boolean hasNext() {
-	return (feature.y() < numLabels);
-    }
-    public Feature next() {
-	featureToReturn.copy(feature);
-	feature.colId++;
-	if (feature.colId >= numCols+addBias) {
-	    feature.colId = 0;
-	    feature._y++;
+	public int numFeatures() {return (numCols+addBias)*numLabels;}
+	public void startScanFeaturesAt(DataSequence data, int pos) {
+		dataRecord = (DataRecord)data;
+		assert (pos == 0);
+		feature.colId = 0;
+		feature._y = 0;
 	}
-	if (featureToReturn.colId == numCols)
-        featureToReturn.val = 1;
-    else
-        featureToReturn.val = dataRecord.getColumn(featureToReturn.colId);
-	return featureToReturn;
-    }
-    /* (non-Javadoc)
-     * @see iitb.CRF.FeatureGenerator#featureName(int)
-     */
-    public String featureName(int featureIndex) {
-        return "ColumnId=" + (featureIndex % (numCols+addBias)) + " label="+(featureIndex/(numCols+addBias));
-    }
-    public int xFeatureIdCurrent() {
-        return feature.colId;
-    }
+	public boolean hasNext() {
+		return (feature.y() < numLabels);
+	}
+	protected int lastColumnIdp1() {return numCols;}
+	protected int firstColumnId() {return 0;}
+	public Feature next() {
+		featureToReturn.copy(feature);
+		feature.colId++;
+		if (feature.colId >= lastColumnIdp1()+addBias) {
+			feature.colId = firstColumnId();
+			feature._y++;
+		}
+		if ((addBias==1) && (featureToReturn.colId == 0))
+			featureToReturn.val = 1;
+		else
+			featureToReturn.val = dataRecord.getColumn(featureToReturn.colId-addBias);
+		return featureToReturn;
+	}
+	/* (non-Javadoc)
+	 * @see iitb.CRF.FeatureGenerator#featureName(int)
+	 */
+	public String featureName(int featureIndex) {
+		return "ColumnId=" + (featureIndex % (numCols+addBias)) + " label="+(featureIndex/(numCols+addBias));
+	}
+	public int xFeatureIdCurrent() {
+		return feature.colId;
+	}
 };
