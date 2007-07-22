ea7e702e222c86e0d057923ccd340e27092da552
sunita <sunita>
2007-07-22 11:51:35 +0000

    *** empty log message ***

--- src/iitb/Model/WordScoreFeatures.java
+++ src/iitb/Model/WordScoreFeatures.java
@@ -13,18 +13,22 @@ import java.io.*;
 public class WordScoreFeatures extends FeatureTypes {
     int stateId;
     int wordPos;
-    
+    int wordCnt;
+    int scoreType;
+    int numScoreType=3;
     WordsInTrain dict;
     public WordScoreFeatures(FeatureGenImpl m, WordsInTrain d) {
 	super(m);
 	dict = d;
     }
-    private void nextStateId() {       
+    private void nextStateId() {
 	stateId = dict.nextStateWithWord(wordPos, stateId);
     }
     public boolean startScanFeaturesAt(DataSequence data, int prevPos, int pos) {
 	stateId = -1;
-	if (dict.count(data.x(pos)) > WordFeatures.RARE_THRESHOLD) {
+    scoreType=0;
+    wordCnt = dict.count(data.x(pos));
+	if (wordCnt > WordFeatures.RARE_THRESHOLD) {
 	    Object token = (data.x(pos));
 	    wordPos = dict.getIndex(token);
 	    stateId = -1;
@@ -37,12 +41,31 @@ public class WordScoreFeatures extends FeatureTypes {
 	return (stateId < model.numStates()) && (stateId >= 0);
     }
     public void next(FeatureImpl f) {
-	setFeatureIdentifier(stateId,stateId,"S",f);
+	if (featureCollectMode())
+        setFeatureIdentifier(stateId*numScoreType+scoreType,stateId,"S_"+scoreType,f);
+    else
+        setFeatureIdentifier(stateId*numScoreType+scoreType,stateId,null,f);
 	f.yend = stateId;
 	f.ystart = -1;
-	f.val = (float)Math.log(((double)dict.count(wordPos,stateId))/dict.count(stateId));
+    switch (scoreType) {
+    case 0:
+        f.val = (float) (1 + Math.log(1+Math.log(dict.count(wordPos,stateId))));
+        break;
+    case 1:
+        f.val = (float) Math.log(1+(double)dict.count(wordPos,stateId)/(double)wordCnt);
+        break;
+    default:
+        f.val = (float)Math.log(1+((double)dict.count(wordPos,stateId))/dict.count(stateId));
+    //f.val = (float) (1 + Math.log(1+Math.log(dict.count(wordPos,stateId))));
+    }
+    //for (int s = 0; s < model.numStates(); s = dict.nextStateWithWord(wordPos, s)) {}
 	// System.out.println(f.toString());
-	nextStateId();
+    if (scoreType<numScoreType-1) {
+        scoreType++;
+    } else {
+        scoreType=0;
+        nextStateId();
+    }
     }
 };
 
