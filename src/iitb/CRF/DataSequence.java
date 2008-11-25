120233a36001350fc2e7a4a2d3162767a87cd280
sunita <sunita>
2008-11-25 12:15:09 +0000

    made serializable

--- src/iitb/CRF/DataSequence.java
+++ src/iitb/CRF/DataSequence.java
@@ -1,4 +1,7 @@
 package iitb.CRF;
+
+import java.io.Serializable;
+
 /**
  * A basic training/test instance needs to support the DataSequence interface.
  * @author Sunita Sarawagi
@@ -6,7 +9,7 @@ package iitb.CRF;
  */ 
 
 
-public interface DataSequence {
+public interface DataSequence extends Serializable {
     public int length();
     public int y(int i);
     /** The type of x is never interpreted by the CRF package. This could be useful for your FeatureGenerator class */ 
