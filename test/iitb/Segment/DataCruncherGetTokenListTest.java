e439c71e8f709016187e9ff524947145ccdb7db8
kaleru <kaleru>
2012-02-08 21:29:53 +0000

    Regression test for DataCruncher.getTokenList.

---
+++ test/iitb/Segment/DataCruncherGetTokenListTest.java
@@ -0,0 +1,42 @@
+/** DataCruncherGetTokenListTest.java
+ * 
+ * @author kampe
+ * @since 1.3
+ * @version 1.3
+ */
+package iitb.Segment;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author kampe
+ *
+ */
+public class DataCruncherGetTokenListTest {
+
+	@Test
+	public void testGetTokenListWithDelimiter() {
+		String tokenString = "South 1/2 Greenwood Avenue, |3";
+		String delimit = ",\t/ -():.;'?#`&\"_";
+		String impDelimit = ",";
+		String[] tokens = DataCruncher.getTokenList(tokenString, delimit, impDelimit);
+		
+		assertEquals(tokens.length, 7);
+		assertEquals(tokens[1], "1");
+		assertEquals(tokens[5], ",");
+	}
+	
+	@Test
+	public void testGetTokenList() {
+		String tokenString = "West Goldfield Avenue, |3";
+		String delimit = ",\t/ -():.;'?#`&\"_";
+		String impDelimit = ",";
+		String[] tokens = DataCruncher.getTokenList(tokenString, delimit, impDelimit);
+
+		assertEquals(tokens.length, 5);
+		assertEquals(tokens[1], "goldfield");
+		assertEquals(tokens[4], "|3");
+	}
+}
