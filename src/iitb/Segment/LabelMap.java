9dab056632dba013ea3c82ec638953dfef57fd93
kaleru <kaleru>
2012-02-09 20:08:27 +0000

    Javadoc and formatting.

--- src/iitb/Segment/LabelMap.java
+++ src/iitb/Segment/LabelMap.java
@@ -1,20 +1,51 @@
-package iitb.Segment;
-/**
- *
+/** LabelMapTest.java
+ * 
  * @author Sunita Sarawagi
- *
- */ 
+ * @version 1.3
+ */
+
+package iitb.Segment;
+
+/**
+ * 
+ * @author Sunita Sarawagi
+ * 
+ */
 
 public class LabelMap {
-    int map(int l) {return l-1;}
-    int revMap(int l) {return l+1;}
-};
+
+	/**
+	 * Decrements the parameter by one.
+	 * @param l Value to be decremented.
+	 * @return l - 1
+	 */
+	public int map(int l) {
+		return l - 1;
+	}
+
+	/**
+	 * Increments the parameter by one.
+	 * @param l Value to be incremented.
+	 * @return l + 1
+	 */
+	public int revMap(int l) {
+		return l + 1;
+	}
+}
 
 class BinaryLabelMap extends LabelMap {
-    int posClass;
-    BinaryLabelMap(int sel) {
-	posClass = sel;
-    }
-    int map(int el) {return (posClass == el)?1:0;}
-    int revMap(int label) {return (label==1)?posClass:0;}
-};
+	
+	int posClass;
+
+	BinaryLabelMap(int sel) {
+		posClass = sel;
+	}
+
+	public int map(int el) {
+		return (posClass == el) ? 1 : 0;
+	}
+
+	public int revMap(int label) {
+		return (label == 1) ? posClass : 0;
+	}
+}
