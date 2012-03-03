6245eebbdaae54a9670fdc66c6c610d01c62431f
kaleru <kaleru>
2012-03-03 14:06:18 +0000

    Organized imports.

--- src/iitb/BSegmentCRF/BFeatureGenerator.java
+++ src/iitb/BSegmentCRF/BFeatureGenerator.java
@@ -1,17 +1,16 @@
-/*
+/** BFeatureGenerator.java
  * Created on Apr 2, 2005
+ * 
+ * @author Sunita Sarawagi
+ * @since 1.2
+ * @version 1.3
  *
  */
 package iitb.BSegmentCRF;
 
 import iitb.CRF.DataSequence;
-import iitb.CRF.FeatureGenerator;
 import iitb.CRF.FeatureGeneratorNested;
 
-/**
- * @author sunita
- *
- */
 public interface BFeatureGenerator extends FeatureGeneratorNested {
     /**
      * @return: the maximum gap between start and end boundary of features
