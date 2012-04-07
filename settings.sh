9ca4402a9401215a3a2b178f8895b8b756d42152
amitjaiswal <amitjaiswal>
2012-04-07 04:33:43 +0000

    Adding default value for CRF_HOME in settings.sh

--- settings.sh
+++ settings.sh
@@ -1,2 +1,16 @@
 # settings for CRF..
-export CLASSPATH=$CRF_HOME/lib/CRF.jar:$CRF_HOME/lib/colt.jar:$CRF_HOME/lib/LBFGS.jar:$CRF_HOME/lib/trove.jar:$CLASSPATH
+
+# Check if CRF_HOME is set 
+if [ ! ${CRF_HOME} ]; then
+  echo "CRF_HOME is not set. Using default as current working directory."
+  export CRF_HOME=.
+fi
+
+echo "Using CRF_HOME=${CRF_HOME}"
+
+# Setting the classpath variable
+
+for file in `ls $CRF_HOME/lib/*.jar`
+do
+  export CLASSPATH=$file:$CLASSPATH
+done
